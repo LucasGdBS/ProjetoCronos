5645c997624b95550ff9a200386401a0700319d7
LucasGdBS <114539692+LucasGdBS@users.noreply.github.com>
2022-11-08 14:35:56 -0300

    Adiciona comentarios explicando cada linha

--- ProjetoCronos/ProjetoCronos.ino
+++ ProjetoCronos/ProjetoCronos.ino
@@ -1,39 +1,42 @@
+//Inclui as Bilbiotecas usadas para o uso do Display LCD
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 
-#define col 16
-#define lin 2
-#define ende 0x3F
+#define col 16 // Define o numero de colunas do LCD usado
+#define lin 2 // Define o numero de linhas do LCD usado
+#define ende 0x3F // Define o endereço do display LCD (pode varia de acordo com a fabricante do display)
 
-//#include <Adafruit_LiquidCrystal.h>
+// Declaração das variáveis usadas no código
+int s = 0; // Variável usada para contagem de segundos
+int count = 0; // Variavel usada para ativar ou desativar o timer
 
-int s = 0;
-int count = 0;
-int button1 = 2;
-int button2 = 3;
-int button3 = 4;
-int button4 = 5;
-int hora, resto, minutos, segundos;
-unsigned long int tempoAnterior1, tempoAnterior2, tempoAnterior3, tempoAnterior4, tempoAnteriorCont;
+int button1 = 2; // Define o pino para o botão 1
+int button2 = 3; // Define o pino para o botão 2
+int button3 = 4; // Define o pino para o botão 3
+int button4 = 5; // Define o pino para o botão 4
 
-LiquidCrystal_I2C lcd_1(ende, col, lin);
+int hora, resto, minutos, segundos; // Define as variáveis de tempo
+
+unsigned long int tempoAnterior1, tempoAnterior2, tempoAnterior3, tempoAnterior4, tempoAnteriorCont; // Cria as variáveis que serão usadas para contar o tempo de ativação dos botões
+
+LiquidCrystal_I2C lcd_1(ende, col, lin); // Cria a variável do LCD
 
 void setup()
 {
-  Serial.begin(96000);
-  lcd_1.init();
-  lcd_1.clear();
-  lcd_1.backlight();
+  
+  lcd_1.init(); // Inicia o LCD
+  lcd_1.clear(); // Limpa o LCD 
+  lcd_1.backlight(); //Ativa a luz de fundo do LCD
   
   
 
-  pinMode(button1, INPUT_PULLUP); //button1 add 1 min
-  pinMode(button2, INPUT_PULLUP); //button2 add 10 min
-  pinMode(button3, INPUT_PULLUP); //button3 add play/pause min
-  pinMode(button4, INPUT_PULLUP); //button4 reset
+  pinMode(button1, INPUT_PULLUP); // button1 adiciona 1 min
+  pinMode(button2, INPUT_PULLUP); // button2 adiciona 10 min
+  pinMode(button3, INPUT_PULLUP); // button3 ativa play/pause do timer
+  pinMode(button4, INPUT_PULLUP); // button4 reseta o timer
 
 }
-void timer(int tempo){
+void timer(int tempo){ // Função para transformar os segundos (s) no formato hora:minutos:segundos 
 
   hora = tempo / 3600;
   resto = tempo % 3600;
@@ -42,9 +45,10 @@ void timer(int tempo){
 
 }
 
-void tela(){
+void tela(){ // Função que vai mostrar na tela do LCD o timer no formato 00:00:00
   
-  if (hora < 10){
+  // Se o tempo em horas for menor que 10 vai printar o número 0 e o numero em seguida
+  if (hora < 10){ 
     lcd_1.print(0);
     lcd_1.print(hora);
   }else{
@@ -70,25 +74,26 @@ void tela(){
 
 void loop()
 {
-lcd_1.setCursor(0, 0);
-  lcd_1.print("Projeto Cronos");
-  lcd_1.setCursor(0, 1);
+  lcd_1.setCursor(0, 0); // Aponta o "ponteiro de escrita" do LCD na COLUNA 0 LINHA 0
+  lcd_1.print("Projeto Cronos"); // Escreve "Projeto Cronos" no local onde está o ponteiro
+  lcd_1.setCursor(0, 1); // Aponmta o "ponteiro de escrita" do LCD na COLUNA 0 LINHA 1
 
-  if (millis() - tempoAnterior1 >= 200){
-    tempoAnterior1 = millis();
-    if (digitalRead(button1) == LOW){
-      s += 60;
+  // A função millis() é um contador de tempo em milisegundos que inicia assim que o arduino é ligado
+  if (millis() - tempoAnterior1 >= 200){ //Só vai entrar nessa condição se o tempo atual (millis()) menos a ultima marcação de tempo (tempoAnterior) for maior que 200 milisegundos
+    tempoAnterior1 = millis(); // Cria uma marcação de tempo
+    if (digitalRead(button1) == LOW){ // Quando o botão for pressionado vai interromper a eletricidade e entrará nessa condição
+      s += 60; // Adiciona 60 segundos (1 minuto)
     }
   }
   if (millis() - tempoAnterior2 >= 200){
     tempoAnterior2 = millis();
     if (digitalRead(button2) == LOW){
-      s += 10*60;
+      s += 10*60; // Adiciona 600 segundos (10 minutos)
     }
   }
-  if (millis() - tempoAnterior3 >= 150){
+  if (millis() - tempoAnterior3 >= 150){ // Vai trocar o valor da variavel count ao ser pressionado
     tempoAnterior3 = millis();
-    if (digitalRead(button3) == LOW){
+    if (digitalRead(button3) == LOW){ 
       if (count == 0){
         count = 1;
       }else{
@@ -98,27 +103,27 @@ lcd_1.setCursor(0, 0);
   }
   if (millis() - tempoAnterior4 >= 200){
     tempoAnterior4 = millis();
-    if (digitalRead(button4) == LOW){
+    if (digitalRead(button4) == LOW){ // Faz o tempo ir para 00 e transforma a Variável count em 0 (pausando o timer)
       s = 0;
       count = 0;
-      lcd_1.clear();
+      lcd_1.clear(); // Limpar o LCD
     }
   }
 
-  if (count == 1){
+  if (count == 1){ // Caso a variavel count seja 1...
     if (s > 0){
-      if (millis() - tempoAnteriorCont >= 1000){
+      if (millis() - tempoAnteriorCont >= 1000){ // VAi entrar nessa condição a cada 1000 milisegundos (1 segundo)...
         tempoAnteriorCont = millis();
-        s--;
+        s--; // E diminuirá nossa variável de tempo (s) em 1
         if (s == 0){
-          count = 0;
+          count = 0; // Ao chegar em 0 vai desativar o contador
         }
       }
     }
   }
 
-  timer(s);
-  tela();
+  timer(s); //Chama a função timer()
+  tela(); // Chama a função tela()
 
  
  
